de81955168db6da936e26b42869802125567fc31
sajidmahmud69 <sajidmahmud136@yahoo.com>
2021-04-12 02:37:02 -0400

    add forgotpassword and reset password functionality in frontend

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -4,15 +4,14 @@ import { Redirect, Route } from 'react-router-dom'
 function PrivateRoute({ component: Component, ...rest}) {
     //  get the component that you want to render and spread the rest of the component
     return (
-        <Route 
-            { ...rest}
-            render =  {(props) => {
-                    localStorage.getItem ("authToken") ? (
-                        <Component {...props}
-                    ) : (
-                        <Redirect to = '/login' />
-                    )
-                } 
+        <Route
+            {...rest}
+            render={(props) =>
+                localStorage.getItem("authToken") ? (
+                <Component {...props} />
+                ) : (
+                <Redirect to="/login" />
+                )
             }
         />
     )
